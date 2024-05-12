66a52cb7df6dff006c20f7dcce2b92b6701d9c26
Weizhuo Zhang <weizhuo.zhang@intel.com>
2024-05-13 00:56:05 +0800

    add profiling script

---
+++ torchvision_models_profile.py
@@ -0,0 +1,40 @@
+import time
+import torch
+import torchvision.models as models
+from torch.profiler import profile, record_function, ProfilerActivity
+from utils import AverageMeter, ProgressMeter
+
+batch_size = 4
+warmup_iterations = 10
+number_iter = 50
+model = models.__dict__["resnet50"](pretrained=True)
+#model = models.__dict__["mobilenet_v3_large"](pretrained=True)
+
+batch_time = AverageMeter('Time', ':6.3f')
+progress = ProgressMeter(
+        number_iter,
+        [batch_time],
+        prefix='Test: ')
+images = torch.randn(batch_size, 3, 224, 224).contiguous(memory_format=torch.channels_last)
+
+for i in range(number_iter):
+    if i > warmup_iterations:
+        end = time.time()
+    if i == warmup_iterations:
+        with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof, record_function("model_inference"):
+            output = model(images)
+    else:
+        output = model(images)
+
+    if i > warmup_iterations:
+        batch_time.update(time.time() - end)
+
+    if i % 10 == 0:
+        progress.display(i)
+
+latency = batch_time.avg / batch_size * 1000
+perf = batch_size / batch_time.avg
+
+print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=-1))
+print('inference latency %.3f ms'%latency)
+print("Throughput: {:.3f} fps".format(perf))
